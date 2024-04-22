bcd4d2467ee8dc42f65e2a99a461fd890881c634
kelvin <ngotho.kelvin@yahoo.com>
2024-04-22 13:03:01 +0300

    Add home paragraph

--- 1Microverse/share_prompts/app/page.jsx
+++ 1Microverse/share_prompts/app/page.jsx
@@ -4,7 +4,14 @@ const Home = () => {
     <section className="w-full flex-center flex-col">
         <h1 className="head_text text-center">
             Discover & Share
-            <br />
+            <br className="max-md:hidden" />
+            <span> AI-Powered Prompts</span>
+            <span className="orange_gradient">
+                <p className="desc text-center">
+                    Promptopia is an open-source AI prompting tool for modern worl to dixocove,
+                     create and share creative prompts
+                </p>
+            </span>
         </h1>
     </section>
   )
